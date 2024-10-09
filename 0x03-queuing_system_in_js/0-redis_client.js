b87780a84c783b14b38f9c1d0a34575f3850b211
Victor Abrokwah <kingportipher@outlook.com>
2024-10-09 22:00:16 +0000

    Task 0 done

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,9 +1,13 @@
 import redis from 'redis';
 
 const client = redis.createClient();
-client.on('connect', () => {
-  console.log('Redis client connected to the server');
-});
+
+// Handle error events
 client.on('error', (err) => {
-  console.log(`Redis client not connected to the server: ${err.message}`);
+    console.log(`Redis client not connected to the server: ${err}`);
+});
+
+// Handle successful connection
+client.on('connect', () => {
+    console.log('Redis client connected to the server');
 });
